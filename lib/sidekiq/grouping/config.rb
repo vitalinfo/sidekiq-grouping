ad3ed63f3c09f270e0cf2b9bc9dd94644cf002ed
Vitalii Riabchynskyi <vital.ryabchinskiy@gmail.com>
2026-01-06 13:49:17 +0000

    Get rid ActiveSupport::Configurable; Fixed read config from file for Sidekiq 7+

--- lib/sidekiq/grouping/config.rb
+++ lib/sidekiq/grouping/config.rb
@@ -3,38 +3,43 @@
 module Sidekiq
   module Grouping
     module Config
-      include ActiveSupport::Configurable
-
-      def self.options
-        if Sidekiq.respond_to?(:[]) # Sidekiq 6.x
-          Sidekiq[:grouping] || {}
-        elsif Sidekiq.respond_to?(:options) # Sidekiq <= 5.x
-          Sidekiq.options[:grouping] || Sidekiq.options["grouping"] || {}
-        else # Sidekiq 7.x
-          Sidekiq.default_configuration[:grouping] || {}
+      class << self
+        %i[lock_ttl max_batch_size poll_interval tests_env].each do |method_name|
+          define_method(method_name) do
+            config[method_name]
+          end
+          define_method(:"#{method_name}=") do |value|
+            config[method_name] = value
+          end
         end
-      end
 
-      # Queue size overflow check polling interval
-      config_accessor :poll_interval do
-        options[:poll_interval] || 3
-      end
+        private
 
-      # Maximum batch size
-      config_accessor :max_batch_size do
-        options[:max_batch_size] || 1000
-      end
+        def config
+          @config ||= ActiveSupport::InheritableOptions.new(
+            lock_ttl: options[:lock_ttl] || 1,
+            max_batch_size: options[:max_batch_size] || 1_000,
+            poll_interval: options[:poll_interval] || 3,
+            tests_env: options[:tests_env] || (
+              defined?(::Rails) && ::Rails.respond_to?(:env) && ::Rails.env.test?
+            )
+          )
+        end
 
-      # Batch queue flush lock timeout
-      config_accessor :lock_ttl do
-        options[:lock_ttl] || 1
-      end
-
-      # Option to override how Sidekiq::Grouping know about tests env
-      config_accessor :tests_env do
-        options[:tests_env] || (
-          defined?(::Rails) && ::Rails.respond_to?(:env) && ::Rails.env.test?
-        )
+        def options
+          @options ||=
+            begin
+              if Sidekiq.respond_to?(:[]) # Sidekiq 6.x
+                Sidekiq[:grouping] || {}
+              elsif Sidekiq.respond_to?(:options) # Sidekiq <= 5.x
+                Sidekiq.options[:grouping] || Sidekiq.options["grouping"] || {}
+              elsif Sidekiq.server?  # Sidekiq >= 7.x
+                Sidekiq::CLI.instance.config[:grouping]
+              else
+                Sidekiq.default_configuration[:grouping] || {}
+              end
+           end
+        end
       end
     end
   end
